27947a86ae2d15e038c53ad8fedb68a94bb98873
TheAndruNique <theandrusyper@gmail.com>
2023-10-30 13:05:12 +0300

    code refactoring

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,8 @@
 from config import *
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from dotenv import load_dotenv
 import os
 
-
-load_dotenv()
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
--- main.py
+++ main.py
@@ -10,4 +10,4 @@ app.register_blueprint(chattings)
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(host='0.0.0.0', port=4000)
+    app.run(host='0.0.0.0')
