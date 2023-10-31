bd15dd17a0d1182b251a09a202d449c6784e7ada
TheAndruNique <theandrusyper@gmail.com>
2023-10-31 12:03:23 +0300

    fixes and code refactoring

--- app/config.py
+++ app/config.py
@@ -1,15 +1,34 @@
 from dotenv import load_dotenv
 import os
+import logging
 
 
 load_dotenv()
 
+def get_int_env_variable(env_name, default, min_value=None, max_value=None):
+    env_value = os.getenv(env_name, default)
+    try:
+        int_value = int(env_value)
+        if (min_value is not None and int_value < min_value) or (max_value is not None and int_value > max_value):
+            raise ValueError("Value is outside the valid range.")
+        return int_value
+    except ValueError:
+        logging.error(f'Error: Environment variable {env_name} must be an integer.')
+        return default
+
 SECRET_KEY = os.getenv('SECRET_KEY')
-MIN_LOGIN_LENGTH = os.getenv('MIN_LOGIN_LENGTH', 3)
-MAX_LOGIN_LENGTH = os.getenv('MIN_LOGIN_LENGTH', 50)
-MIN_PASSWORD_LENGTH = os.getenv('MIN_PASSWORD_LENGTH', 5)
+
+required_values = ["SECRET_KEY"]
+missing_values = [value for value in required_values if os.environ.get(value) is None]
+if len(missing_values) > 0:
+      logging.error(f"The following environment values are missing in your .env: {', '.join(missing_values)}")
+      exit(1)
+
+MIN_LOGIN_LENGTH = get_int_env_variable('MIN_LOGIN_LENGTH', 3)
+MAX_LOGIN_LENGTH = get_int_env_variable('MAX_LOGIN_LENGTH', 50)
+MIN_PASSWORD_LENGTH = get_int_env_variable('MIN_PASSWORD_LENGTH', 5)
 SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI', 'sqlite:///users.db')
-MONGODB_CONNECTION_URI = os.getenv('MONGODB_URI')
+MONGODB_CONNECTION_URI = os.getenv('MONGODB_URI', 'mongodb://localhost:27017')
 BASE_PATH = os.getenv('BASE_PATH', '/api/v1.0')
-TOKEN_LIFETIME = os.getenv('TOKEN_LIFETIME', 3600)
-PORT = os.getenv('PORT', 5000)
+TOKEN_LIFETIME = get_int_env_variable('TOKEN_LIFETIME', 3600)
+PORT = get_int_env_variable('PORT', 5000)
